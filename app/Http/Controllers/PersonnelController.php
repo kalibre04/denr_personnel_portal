97619dbba745237d9054375641c33bf62daf0143
kalibre04 <82302756+kalibre04@users.noreply.github.com>
2022-04-13 11:30:31 +0800

    Update PersonnelController.php

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Auth;
+use Illuminate\Support\Facades\Validator;
+use Session;
 
 use App\Models\User;
 
@@ -22,6 +24,23 @@ class PersonnelController extends Controller
         }
     }
 
+    public function profilepasswordupdate(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'password'         => 'required', 
+            'confirm_password' => 'required|same:password', 
+        ]);
+        if ($validator->fails()) { 
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();          
+        }
+        $user = User::find($id);
+        $user->password = bcrypt($request->password);
 
+        $user->save();
+        Session::flash('flash_message','Your Profile Was Successfully Updated');
+        return redirect()->back();
+    }
 
 }
